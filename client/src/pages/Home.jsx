711977d9666600d335bf6ec4191f2284ccb0deb9
Yusri <yusripawaki2@gmail.com>
2023-04-19 00:05:05 +0800

    fix: module issue

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,24 +1,10 @@
-import React, { useState } from "react";
-import MainLayout from "../layout/MainLayout";
-import Room from "../components/webrtc/Room";
-import { io } from "socket.io-client";
-
-const socket = io("ws://localhost:3002");
+import React from 'react'
+import MainLayout from '../layout/MainLayout'
 
 export default function Home() {
-  // send a message to the server
-  socket.emit("hello from client", 5, "6", { 7: Uint8Array.from([8]) });
-
-  // receive a message from the server
-  socket.on("hello from server", (...args) => {
-    // ...
-  });
-
   return (
     <>
-      <MainLayout>
-        <Room />
-      </MainLayout>
+    <MainLayout />
     </>
-  );
+  )
 }
