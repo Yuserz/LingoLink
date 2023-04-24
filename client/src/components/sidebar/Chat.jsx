af8f4631807343d0df723c9ac92dc73d274ce5e9
Yusri <yusripawaki2@gmail.com>
2023-04-25 01:26:01 +0800

    feat: working contact list

--- client/src/components/sidebar/Chat.jsx
+++ client/src/components/sidebar/Chat.jsx
@@ -1,15 +1,26 @@
-import React, {  useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import arrowDown from "../../assets/icons/arrowDown.svg";
 import add from "../../assets/icons/add.svg";
 import AddContact from "./modals/AddContact";
-
+import { MyDataContext } from "../../pages/Home";
 
 export default function Chat() {
   const [expanded, setExpanded] = useState(false);
   const [showModal, setShowModal] = useState(false);
- 
-  
-  // console.log
+  const [contacts, setContact] = useState();
+  const data = useContext(MyDataContext);
+
+  useEffect(() => {
+    if (data.data) {
+      console.log(data.data);
+      setContact(data.data.contacts);
+    }
+  }, [data]);
+
+  // useEffect(() => {
+  //   console.log("contacts:", contacts);
+  //   // setContact(data.contacts)
+  // }, [contacts]);
 
   const toggleExpanded = () => {
     setExpanded(!expanded);
@@ -17,7 +28,7 @@ export default function Chat() {
 
   return (
     <div
-      className={`chat-container bg-secondary rounded-md p-3 px-4 flex justify-between${
+      className={`chat-container  bg-secondary rounded-md p-3 px-4 flex flex-col justify-between${
         expanded ? "expanded min-h-[150px] max-h-fit" : ""
       }`}
     >
@@ -43,8 +54,17 @@ export default function Chat() {
         >
           <img src={add} alt="" />
         </button>
+
         {showModal ? <AddContact closeModal={setShowModal} /> : ""}
       </div>
+      <div>
+        {contacts
+          ? contacts.map((contact, index) => (
+              <div key={index}>{contact.name}</div>
+            ))
+          : []}
+      </div>
+      {/* {data.data.name} */}
     </div>
   );
 }
