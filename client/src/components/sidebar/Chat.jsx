63dfafedd82fb4744ea2ed97488ab11db199c697
Yusri <yusripawaki2@gmail.com>
2023-05-03 21:13:06 +0800

    feat: add contact list

--- client/src/components/sidebar/Chat.jsx
+++ client/src/components/sidebar/Chat.jsx
@@ -4,6 +4,7 @@ import add from "../../assets/icons/add.svg";
 import AddContact from "../modals/AddContact";
 import { getContact } from "../../api/api";
 import { MyGlobalContext } from "../../context/MyGlobalContext";
+import ContactList from "./ContactList";
 
 export default function Chat() {
   const [expanded, setExpanded] = useState(false);
@@ -67,23 +68,7 @@ export default function Chat() {
 
         {showModal ? <AddContact closeModal={setShowModal} /> : null}
       </div>
-      <div className="flex flex-col gap-2  border-gray-300/80 border-t pt-2">
-        {loading ? ( // show a loading indicator or a message if data is being fetched
-          <h1>Loading contacts...</h1>
-        ) : contacts.length > 0 ? (
-          contacts.map((contact, index) => (
-            <button
-              className="flex gap-2 px-3 p-1 hover:shadow-sm hover:border-2 hover:border-primary/20 rounded-md "
-              key={index}
-            >
-              <h2>{index + 1 + "."}</h2>
-              <h2>{contact.name}</h2>
-            </button>
-          ))
-        ) : (
-          <h1 className="opacity-70">No contact</h1>
-        )}
-      </div>
+      <ContactList contacts={contacts} loading={loading} />
     </div>
   );
 }
